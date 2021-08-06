b4b4071140fc5ff009085e87efc6437fb7a92fc5
Yagnya Dutta Dhal <yagnya87@gmail.com>
2021-08-06 10:14:43 +0530

    service registry related issue,spring-starter-web dependendencyand @enablediscoveryclient added

--- user-service/src/main/java/com/cakekart/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/cakekart/userservice/UserServiceApplication.java
@@ -2,10 +2,12 @@ package com.cakekart.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackages = "com.cakekart.userservice.repository")
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
